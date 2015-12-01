6e6806a7a7c20f9fc7840a95642cd7f3dbf649cd
mRokita <mrokita@mrokita.pl>
2015-12-01 22:27:48 +0100

    Rozwijam layout aplikacji

--- app/src/main/java/pl/mrokita/mojeokienko/MojeOkienkoActivity.java
+++ app/src/main/java/pl/mrokita/mojeokienko/MojeOkienkoActivity.java
@@ -2,11 +2,17 @@ package pl.mrokita.mojeokienko;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -14,44 +20,101 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 
 public class MojeOkienkoActivity extends AppCompatActivity {
     private GoogleMap map;
+    private Toolbar mToolbar;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moje_okienko);
-        Fragment fragment = new NewTicketFragment();
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fl_content, fragment);
-        fragmentTransaction.commit();
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        if(getSupportActionBar()!=null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+            setupDrawer();
+        }
+        View statusBar = findViewById(R.id.statusbar);
+        statusBar.setVisibility(Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT ?
+                View.VISIBLE : View.GONE);
+        setupFragment();
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        setupNavigationView();
     }
 
+    private void setupNavigationView(){
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                if (menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+                mDrawerLayout.closeDrawers();
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.drawer_map:
+                        MapFragment mapFragment = new MapFragment();
+                        mapFragment.getMapAsync(new OnMapReadyCallback() {
+                            @Override
+                            public void onMapReady(GoogleMap googleMap) {
+                                googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                            }
+                        });
+                        fragment = mapFragment;
+                        addFragment(fragment);
+                        return true;
+                    case R.id.drawer_settings:
+                        Intent intent = new Intent(MojeOkienkoActivity.this, PrefsActivity.class);
+                        startActivity(intent);
+                }
+                return false;
+            }
+        });
+    }
+    private void setupFragment(){
+        Fragment fragment = new NewTicketFragment();
+        addFragment(fragment);
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                mToolbar, R.string.drawer_open, R.string.drawer_close) {
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+    }
+
+    public void addFragment(Fragment fragment){
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                       .replace(R.id.fl_content, fragment)
+                .commit();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_moje_okienko, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         if (id == R.id.action_settings) {
-            MapFragment fragment = new MapFragment();
-            fragment.getMapAsync(new OnMapReadyCallback() {
-                @Override
-                public void onMapReady(GoogleMap googleMap) {
-                    googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-                }
-            });
-            FragmentManager fragmentManager = getFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.fl_content, fragment);
-            fragmentTransaction.commit();
             return true;
         }
 
---
+++ app/src/main/java/pl/mrokita/mojeokienko/PrefsActivity.java
@@ -0,0 +1,32 @@
+package pl.mrokita.mojeokienko;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.MenuItem;
+
+public class PrefsActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prefs);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setTitle(getString(R.string.drawer_settings));
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fl_content, new PrefsFragment())
+                .commit();
+        View statusBar = findViewById(R.id.statusbar);
+        statusBar.setVisibility(Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT ?
+                View.VISIBLE : View.GONE);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        finish();
+        return true;
+    }
+}
